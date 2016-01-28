0f55d3c034fdcd22039a415bdfec073adbe0aaa6
Jason Quense <monastic.panic@gmail.com>
2016-01-28 09:10:08 -0500

    [fixed] add column cellClass to header

--- src/HeaderCell.js
+++ src/HeaderCell.js
@@ -33,7 +33,9 @@ var HeaderCell = React.createClass({
       'react-grid-HeaderCell--resizing': this.state.resizing,
       'react-grid-HeaderCell--locked': this.props.column.locked
     });
-    className = joinClasses(className, this.props.className);
+
+    className = joinClasses(className, this.props.className, this.props.column.cellClass);
+
     var cell = this.getCell();
     return (
       <div className={className} style={this.getStyle()}>
